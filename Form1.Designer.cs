462b074c818f2f20567d6da6683d1652a9e29c70
Paul Mystakidis <sabicera@gmail.com>
2024-07-31 01:31:46 +0300

    15131

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,79 +33,83 @@ namespace Compare
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             K9RichTextBox = new RichTextBox();
             UWRichTextBox = new RichTextBox();
-            Check_Btn = new Button();
             LB_K9 = new Label();
             LB_UW = new Label();
             K9_Clear_Btn = new Button();
             UW_Clear_Btn = new Button();
+            Btn_CopyK9 = new Button();
+            Btn_CopyUW = new Button();
+            Btn_Check = new Button();
+            LB_Panama = new Label();
+            LB_Time = new Label();
             SuspendLayout();
             // 
             // K9RichTextBox
             // 
+            K9RichTextBox.AcceptsTab = true;
             K9RichTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             K9RichTextBox.BackColor = SystemColors.Info;
             K9RichTextBox.BorderStyle = BorderStyle.None;
-            K9RichTextBox.Location = new Point(12, 71);
+            K9RichTextBox.EnableAutoDragDrop = true;
+            K9RichTextBox.Font = new Font("Calibri", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            K9RichTextBox.Location = new Point(12, 55);
             K9RichTextBox.Name = "K9RichTextBox";
-            K9RichTextBox.Size = new Size(374, 378);
+            K9RichTextBox.Size = new Size(555, 744);
             K9RichTextBox.TabIndex = 0;
             K9RichTextBox.Text = "";
+            K9RichTextBox.WordWrap = false;
             K9RichTextBox.ContextMenuStripChanged += RichTextContextMenu;
+            K9RichTextBox.TextChanged += K9RichTextBox_TextChanged;
             K9RichTextBox.MouseDown += K9RichTextBox_MouseDown;
             // 
             // UWRichTextBox
             // 
+            UWRichTextBox.AcceptsTab = true;
             UWRichTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             UWRichTextBox.BackColor = SystemColors.Info;
             UWRichTextBox.BorderStyle = BorderStyle.None;
-            UWRichTextBox.Location = new Point(418, 71);
+            UWRichTextBox.EnableAutoDragDrop = true;
+            UWRichTextBox.Font = new Font("Calibri", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            UWRichTextBox.Location = new Point(578, 55);
             UWRichTextBox.Name = "UWRichTextBox";
-            UWRichTextBox.Size = new Size(374, 378);
+            UWRichTextBox.Size = new Size(555, 744);
             UWRichTextBox.TabIndex = 1;
             UWRichTextBox.Text = "";
+            UWRichTextBox.WordWrap = false;
+            UWRichTextBox.TextChanged += UWRichTextBox_TextChanged;
             UWRichTextBox.MouseDown += UWRichTextBox_MouseDown;
-            // 
-            // Check_Btn
-            // 
-            Check_Btn.BackColor = SystemColors.ActiveCaption;
-            Check_Btn.FlatStyle = FlatStyle.Flat;
-            Check_Btn.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            Check_Btn.Location = new Point(341, 9);
-            Check_Btn.Name = "Check_Btn";
-            Check_Btn.Size = new Size(119, 48);
-            Check_Btn.TabIndex = 2;
-            Check_Btn.Text = "Check";
-            Check_Btn.UseVisualStyleBackColor = false;
-            Check_Btn.Click += Check_Btn_Click;
             // 
             // LB_K9
             // 
             LB_K9.AutoSize = true;
-            LB_K9.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            LB_K9.Location = new Point(180, 38);
+            LB_K9.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            LB_K9.ForeColor = Color.White;
+            LB_K9.Location = new Point(387, 9);
             LB_K9.Name = "LB_K9";
-            LB_K9.Size = new Size(39, 30);
+            LB_K9.Size = new Size(50, 38);
             LB_K9.TabIndex = 3;
             LB_K9.Text = "K9";
             // 
             // LB_UW
             // 
             LB_UW.AutoSize = true;
-            LB_UW.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            LB_UW.Location = new Point(540, 38);
+            LB_UW.Font = new Font("Comic Sans MS", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            LB_UW.ForeColor = Color.White;
+            LB_UW.Location = new Point(771, 8);
             LB_UW.Name = "LB_UW";
-            LB_UW.Size = new Size(131, 30);
+            LB_UW.Size = new Size(169, 38);
             LB_UW.TabIndex = 4;
             LB_UW.Text = "Underwater";
             // 
             // K9_Clear_Btn
             // 
-            K9_Clear_Btn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            K9_Clear_Btn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             K9_Clear_Btn.BackColor = Color.FromArgb(255, 128, 128);
             K9_Clear_Btn.FlatStyle = FlatStyle.Flat;
-            K9_Clear_Btn.Location = new Point(311, 426);
+            K9_Clear_Btn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            K9_Clear_Btn.Location = new Point(12, 763);
             K9_Clear_Btn.Name = "K9_Clear_Btn";
-            K9_Clear_Btn.Size = new Size(75, 23);
+            K9_Clear_Btn.Size = new Size(75, 36);
             K9_Clear_Btn.TabIndex = 7;
             K9_Clear_Btn.Text = "Delete";
             K9_Clear_Btn.UseVisualStyleBackColor = false;
@@ -116,31 +120,101 @@ namespace Compare
             UW_Clear_Btn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             UW_Clear_Btn.BackColor = Color.FromArgb(255, 128, 128);
             UW_Clear_Btn.FlatStyle = FlatStyle.Flat;
-            UW_Clear_Btn.Location = new Point(418, 426);
+            UW_Clear_Btn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            UW_Clear_Btn.Location = new Point(578, 763);
             UW_Clear_Btn.Name = "UW_Clear_Btn";
-            UW_Clear_Btn.Size = new Size(75, 23);
+            UW_Clear_Btn.Size = new Size(75, 36);
             UW_Clear_Btn.TabIndex = 8;
             UW_Clear_Btn.Text = "Delete";
             UW_Clear_Btn.UseVisualStyleBackColor = false;
             UW_Clear_Btn.Click += UW_Clear_Btn_Click;
             // 
+            // Btn_CopyK9
+            // 
+            Btn_CopyK9.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            Btn_CopyK9.BackColor = Color.FromArgb(192, 255, 255);
+            Btn_CopyK9.FlatStyle = FlatStyle.Flat;
+            Btn_CopyK9.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_CopyK9.Location = new Point(452, 763);
+            Btn_CopyK9.Name = "Btn_CopyK9";
+            Btn_CopyK9.Size = new Size(115, 36);
+            Btn_CopyK9.TabIndex = 9;
+            Btn_CopyK9.Text = "Copy K9";
+            Btn_CopyK9.UseVisualStyleBackColor = false;
+            Btn_CopyK9.Click += Btn_CopyK9_Click;
+            // 
+            // Btn_CopyUW
+            // 
+            Btn_CopyUW.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            Btn_CopyUW.BackColor = Color.FromArgb(192, 255, 255);
+            Btn_CopyUW.FlatStyle = FlatStyle.Flat;
+            Btn_CopyUW.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_CopyUW.Location = new Point(1018, 763);
+            Btn_CopyUW.Name = "Btn_CopyUW";
+            Btn_CopyUW.Size = new Size(115, 36);
+            Btn_CopyUW.TabIndex = 10;
+            Btn_CopyUW.Text = "Copy U/W";
+            Btn_CopyUW.UseVisualStyleBackColor = false;
+            Btn_CopyUW.Click += Btn_CopyUW_Click;
+            // 
+            // Btn_Check
+            // 
+            Btn_Check.Anchor = AnchorStyles.Top;
+            Btn_Check.BackColor = Color.FromArgb(192, 255, 255);
+            Btn_Check.FlatStyle = FlatStyle.Flat;
+            Btn_Check.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Btn_Check.Location = new Point(504, 8);
+            Btn_Check.Name = "Btn_Check";
+            Btn_Check.Size = new Size(137, 36);
+            Btn_Check.TabIndex = 11;
+            Btn_Check.Text = "Check";
+            Btn_Check.UseVisualStyleBackColor = false;
+            Btn_Check.Click += Btn_Check_Click;
+            // 
+            // LB_Panama
+            // 
+            LB_Panama.AutoSize = true;
+            LB_Panama.Font = new Font("Calibri", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            LB_Panama.ForeColor = Color.White;
+            LB_Panama.Location = new Point(12, 13);
+            LB_Panama.Name = "LB_Panama";
+            LB_Panama.Size = new Size(138, 26);
+            LB_Panama.TabIndex = 12;
+            LB_Panama.Text = "Panama Now :";
+            // 
+            // LB_Time
+            // 
+            LB_Time.AutoSize = true;
+            LB_Time.Font = new Font("Calibri", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            LB_Time.ForeColor = Color.Yellow;
+            LB_Time.Location = new Point(156, 13);
+            LB_Time.Name = "LB_Time";
+            LB_Time.Size = new Size(106, 26);
+            LB_Time.TabIndex = 13;
+            LB_Time.Text = "Date & Time";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ControlDark;
-            ClientSize = new Size(804, 461);
+            BackColor = Color.Teal;
+            ClientSize = new Size(1145, 811);
+            Controls.Add(LB_Time);
+            Controls.Add(LB_Panama);
+            Controls.Add(Btn_Check);
+            Controls.Add(Btn_CopyUW);
+            Controls.Add(Btn_CopyK9);
             Controls.Add(UW_Clear_Btn);
             Controls.Add(K9_Clear_Btn);
             Controls.Add(LB_UW);
             Controls.Add(LB_K9);
-            Controls.Add(Check_Btn);
             Controls.Add(UWRichTextBox);
             Controls.Add(K9RichTextBox);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            MaximumSize = new Size(820, 1000);
-            MinimumSize = new Size(820, 500);
+            MaximumSize = new Size(1161, 2000);
+            MinimumSize = new Size(1161, 850);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Inspections Checker";
             FormClosing += Form1_FormClosing;
             Load += Form1_Load;
@@ -153,10 +227,14 @@ namespace Compare
 
         private RichTextBox K9RichTextBox;
         private RichTextBox UWRichTextBox;
-        private Button Check_Btn;
         private Label LB_K9;
         private Label LB_UW;
         private Button K9_Clear_Btn;
         private Button UW_Clear_Btn;
+        private Button Btn_CopyK9;
+        private Button Btn_CopyUW;
+        private Button Btn_Check;
+        private Label LB_Panama;
+        private Label LB_Time;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Compare
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer;
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // Set the timer interval to 1 second (1000 milliseconds)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DisplayPanamaTime();
+        }
         public Form1()
         {
             InitializeComponent();
+            DisplayPanamaTime();
+            InitializeTimer();
+
+            // Subscribe to the KeyDown event for both RichTextBox controls
+            K9RichTextBox.KeyDown += new KeyEventHandler(RichTextBox_KeyDown);
+            UWRichTextBox.KeyDown += new KeyEventHandler(RichTextBox_KeyDown);
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -36,6 +51,73 @@ namespace Compare
 
             File.WriteAllText(K9FilePath, K9RichTextBox.Text);
             File.WriteAllText(UWFilePath, UWRichTextBox.Text);
+        }
+        private void DisplayPanamaTime()
+        {
+            try
+            {
+                // Define the Panama time zone (Eastern Standard Time, UTC-5 without DST)
+                TimeZoneInfo panamaTimeZone = TimeZoneInfo.CreateCustomTimeZone("Panama Standard Time", TimeSpan.FromHours(-5), "Panama Standard Time", "Panama Standard Time");
+
+                // Get current UTC time
+                DateTime utcNow = DateTime.UtcNow;
+
+                // Convert UTC time to Panama local time
+                DateTime panamaLocalTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, panamaTimeZone);
+
+                // Format the Panama local time as "DD/MM HH:MM"
+                string formattedPanamaTime = panamaLocalTime.ToString("dd/MM HH:mm:ss");
+
+                // Display the Panama local time on the label
+                LB_Time.Text = $"{formattedPanamaTime}";
+            }
+            catch (Exception ex)
+            {
+                LB_Time.Text = $"An error occurred: {ex.Message}";
+            }
+        }
+        // Override ProcessCmdKey to handle the Tab key
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Tab)
+            {
+                // Handle the Tab key
+                Control focusedControl = ActiveControl as RichTextBox;
+                if (focusedControl != null && focusedControl is RichTextBox)
+                {
+                    RichTextBox richTextBox = (RichTextBox)focusedControl;
+
+                    // Insert a tab character
+                    int position = richTextBox.SelectionStart;
+                    richTextBox.Text = richTextBox.Text.Insert(position, "\t");
+                    richTextBox.SelectionStart = position + 1;
+
+                    // Suppress the default behavior of Tab key
+                    return true;
+                }
+            }
+            else if (keyData == (Keys.Control | Keys.Z))
+            {
+                // Handle Ctrl+Z for undo
+                Control focusedControl = ActiveControl as RichTextBox;
+                if (focusedControl != null && focusedControl is RichTextBox)
+                {
+                    RichTextBox richTextBox = (RichTextBox)focusedControl;
+
+                    // Perform undo operation
+                    if (richTextBox.CanUndo)
+                    {
+                        richTextBox.Undo();
+                        // Suppress the default behavior of Ctrl+Z
+                        return true;
+                    }
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void RichTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Optional: Additional key handling can be added here if needed
         }
         private void RichTextContextMenu(object sender, EventArgs e) { }
         private void K9RichTextBox_MouseDown(object sender, MouseEventArgs e)
@@ -80,7 +162,7 @@ namespace Compare
                 menu.Show(UWRichTextBox, e.Location);
             }
         }
-        private void Check_Btn_Click(object sender, EventArgs e)
+        private void CompareRichTextBoxes()
         {
             // Clear previous highlights
             ClearHighlights(K9RichTextBox);
@@ -105,8 +187,8 @@ namespace Compare
                         matchedLines.Add(lines1[i]);
                         matchedLines1.Add(i);
                         matchedLines2.Add(j);
-                        HighlightLine(K9RichTextBox, i, Color.LightGreen);
-                        HighlightLine(UWRichTextBox, j, Color.LightGreen);
+                        HighlightLine(K9RichTextBox, i, Color.LightBlue);
+                        HighlightLine(UWRichTextBox, j, Color.LightBlue);
                         break;
                     }
                 }
@@ -127,6 +209,10 @@ namespace Compare
                     HighlightLine(UWRichTextBox, j, Color.LightCoral);
                 }
             }
+        }
+        private void RichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CompareRichTextBoxes();
         }
         private void ClearHighlights(RichTextBox richTextBox)
         {
@@ -153,6 +239,80 @@ namespace Compare
         private void UW_Clear_Btn_Click(object sender, EventArgs e)
         {
             UWRichTextBox.Clear();
+        }
+        private void K9RichTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void UWRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_CopyK9_Click(object sender, EventArgs e)
+        {
+            // Check if K9RichTextBox is not empty
+            if (!string.IsNullOrWhiteSpace(K9RichTextBox.Text))
+            {
+                // Copy the text content of K9RichTextBox to the clipboard
+                Clipboard.SetText(K9RichTextBox.Text);
+            }
+        }
+        private void Btn_CopyUW_Click(object sender, EventArgs e)
+        {
+            // Check if UWRichTextBox is not empty
+            if (!string.IsNullOrWhiteSpace(UWRichTextBox.Text))
+            {
+                // Copy the text content of UWRichTextBox to the clipboard
+                Clipboard.SetText(UWRichTextBox.Text);
+            }
+        }
+        private void Btn_Check_Click(object sender, EventArgs e)
+        {
+            // Clear previous highlights
+            ClearHighlights(K9RichTextBox);
+            ClearHighlights(UWRichTextBox);
+
+            // Get the lines from each RichTextBox
+            string[] lines1 = K9RichTextBox.Lines;
+            string[] lines2 = UWRichTextBox.Lines;
+
+            // Create sets to keep track of matched and unmatched lines
+            HashSet<string> matchedLines = new HashSet<string>();
+            HashSet<int> matchedLines1 = new HashSet<int>();
+            HashSet<int> matchedLines2 = new HashSet<int>();
+
+            // Find matching lines
+            for (int i = 0; i < lines1.Length; i++)
+            {
+                for (int j = 0; j < lines2.Length; j++)
+                {
+                    if (!matchedLines2.Contains(j) && lines1[i] == lines2[j])
+                    {
+                        matchedLines.Add(lines1[i]);
+                        matchedLines1.Add(i);
+                        matchedLines2.Add(j);
+                        HighlightLine(K9RichTextBox, i, Color.LightBlue);
+                        HighlightLine(UWRichTextBox, j, Color.LightBlue);
+                        break;
+                    }
+                }
+            }
+            // Count differences in lines1 that are not in lines2
+            for (int i = 0; i < lines1.Length; i++)
+            {
+                if (!matchedLines1.Contains(i))
+                {
+                    HighlightLine(K9RichTextBox, i, Color.LightCoral);
+                }
+            }
+            // Count additions and differences in lines2 that are not in lines1
+            for (int j = 0; j < lines2.Length; j++)
+            {
+                if (!matchedLines2.Contains(j))
+                {
+                    HighlightLine(UWRichTextBox, j, Color.LightCoral);
+                }
+            }
         }
     }
 }
